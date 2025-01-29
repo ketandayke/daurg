c82e77aa794296d8131de922127824faa15eba80
ketandayke <ketandayke@gmail.com>
2025-01-29 19:03:37 +0530

    changed limit

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ app.use(
   );
 
 app.use(cookieParser());
-app.use(express.json({limit:"16kb"}));
+app.use(express.json({limit:"10mb"}));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 
